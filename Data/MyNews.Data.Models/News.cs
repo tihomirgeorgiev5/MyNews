0c1f0a5524aae0d3fda836fa553e491be05194df
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-22 01:20:47 +0200

    added class News

---
+++ Data/MyNews.Data.Models/News.cs
@@ -0,0 +1,27 @@
+﻿namespace MyNews.Data.Models
+{
+    using System;
+
+    using MyNews.Data.Common.Models;
+
+    public class News : BaseDeletableModel<int>
+    {
+        private int writerCount;
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int WriterCount
+        {
+            get { return this.writerCount; }
+            set { this.writerCount = value; }
+        }
+
+        public string AddedByUserId { get; set; }
+
+        public virtual ApplicationUser AddedByUser { get; set; }
+    }
+}
