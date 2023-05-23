e222d156471148aebcdf0a426e68e4b20849a979
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-24 01:16:47 +0200

    added class ArticleTag

---
+++ Data/MyNews.Data.Models/ArticleTag.cs
@@ -0,0 +1,15 @@
+﻿namespace MyNews.Data.Models
+{
+    public class ArticleTag
+    {
+        public int Id { get; set; }
+
+        public int ArticleId { get; set; }
+
+        public virtual NewsArticle Article { get; set; }
+
+        public int TagsId { get; set; }
+
+        public virtual NewsTag Tag { get; set; }
+    }
+}
