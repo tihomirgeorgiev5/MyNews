2ea7ae424c31014be40a99e4ea99170cbe5c07b6
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-24 01:07:05 +0200

    added class NewsTag

---
+++ Data/MyNews.Data.Models/NewsTag.cs
@@ -0,0 +1,9 @@
+﻿namespace MyNews.Data.Models
+{
+    using MyNews.Data.Common.Models;
+
+    public class NewsTag : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+    }
+}
