c4179a3276885ad0b441a2de29101cb74341d369
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-23 23:43:30 +0200

    added class NewsCategory

---
+++ Data/MyNews.Data.Models/NewsCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace MyNews.Data.Models
+{
+    using MyNews.Data.Common.Models;
+
+    public class NewsCategory : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
