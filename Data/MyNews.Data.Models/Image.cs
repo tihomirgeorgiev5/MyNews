8d10a99f81386673764f7ed9cff6a93cd261800d
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-25 20:31:18 +0200

    added class Image

---
+++ Data/MyNews.Data.Models/Image.cs
@@ -0,0 +1,30 @@
+﻿namespace MyNews.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using MyNews.Data.Common.Models;
+
+    public class Image : BaseModel<string>
+    {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public int ArticlesId { get; set; }
+
+        public virtual NewsArticle Article { get; set; }
+
+        public string Extension { get; set; }
+
+        //// The contents of the image is in the file system
+
+        public string AddedByUserId { get; set; }
+
+        public ApplicationUser AddedByUser { get; set; }   
+    }
+}
