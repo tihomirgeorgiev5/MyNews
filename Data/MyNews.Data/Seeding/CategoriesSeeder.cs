c6e3552323ebc8ed20202c8c7374fe83db5b3e0e
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-28 11:11:20 +0200

    added CategoriesSeeder and Bootstrap for it

---
+++ Data/MyNews.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,25 @@
+﻿namespace MyNews.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using MyNews.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.NewsCategories.Any())
+            {
+                return;
+            }
+
+            await dbContext.NewsCategories.AddAsync(new NewsCategory { Name = "България" });
+            await dbContext.NewsCategories.AddAsync(new NewsCategory { Name = "Свят" });
+            await dbContext.NewsCategories.AddAsync(new NewsCategory { Name = "Варна" });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
