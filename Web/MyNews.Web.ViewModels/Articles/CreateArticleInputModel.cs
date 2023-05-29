35f04398ba975e7dfcf64bcc02feba13bd9913e9
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-29 09:42:00 +0200

    Added input models in Article

--- Web/MyNews.Web.ViewModels/Articles/CreateArticleInputModel.cs
+++ Web/MyNews.Web.ViewModels/Articles/CreateArticleInputModel.cs
@@ -1,7 +1,24 @@
 ﻿namespace MyNews.Web.ViewModels.Articles
 {
+    using System;
+    using System.Collections.Generic;
+
+    using MyNews.Data.Models;
+
     public class CreateArticleInputModel
     {
+        public string Title { get; set; }
 
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<NewsCommentsInputModel> NewsComments { get; set; }
     }
 }
---
+++ Web/MyNews.Web.ViewModels/Articles/NewsCommentsInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyNews.Web.ViewModels.Articles
+{
+    public class NewsCommentsInputModel
+    {
+        public string NewsArticle { get; set; }
+
+        public int CommentsCount { get; set; }
+    }
+}
